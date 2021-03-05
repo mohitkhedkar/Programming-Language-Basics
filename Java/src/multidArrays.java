b7d2291af807039b9cbe4fdd8c1bbe17dce96266
mohitkhedkar <mohitkhedkar521@gmail.com>
2021-03-05 23:51:59 +0530

    update

---
+++ Java/src/multidArrays.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class multidArrays {
+    public static void main(String[] args) {
+        int[][] numbers = {{1,2,3},{2,34,5}};
+        System.out.println(Arrays.deepToString(numbers));
+    }
+}
---
+++ Java/src/userInput.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class userInput {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int age = scanner.nextInt();
+        System.out.println(age);
+    }
+
+}
